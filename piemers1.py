7d1583cf462e11ad2156a50924a72225a8549b7b
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-01-15 12:59:23 +0200

    Update apps

--- piemers1.py
+++ piemers1.py
@@ -6,9 +6,21 @@ class Product():
     def get_total_price(self):
         total_price = self.price*self.daudzums
         return total_price
-
+    
+class PirkumuGrozs():
+    def pievienot_grozam(self,product):
+        print("Produkts",product.name,"pievienots grozam!")
+    def izdests_no_grizam(self,product):
+        print("Produkts",product.name,"nodzēsts no groza!")
+    def get_total_price(self,product):
+        print("Kopējā simma",product.get_total_price()) 
+        
 
 Olas = Product("Balticovo",0.17,12)
 Maize = Product("Rupjmaize",2.40,2)
 print(Olas.get_total_price())
 print(Maize.get_total_price())
+
+Pirkumi = PirkumuGrozs()
+Pirkumi.pievienot_grozam(Olas)
+Pirkumi.get_total_price(Olas)
