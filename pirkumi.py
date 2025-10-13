ef3860409a0648381599f79be2ca3d5f1d03f98a
Aleksandr Zelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2025-10-13 17:44:57 +0300

    Update apps

--- pirkumi.py
+++ pirkumi.py
@@ -8,50 +8,22 @@ class Product():
         total_price = self.price*self.daudzums
         return total_price
 
-class IepirkumuGrozs():
-    def add_product_to_cart(self,product):
-        print("Produkts",product.name,"Pievienots grozam!")
-    def remove_product_from_cart(self,product):
-        print("Produkts",product.name,"nodzēsts no groza!")
-    def get_total_price(self,product):
-        total_price = product.get_total_price()
-        print("Kopējē summa:",total_price)
-
 
 Olas = Product("Olas",0.20,10)
 Piens = Product("Piens",1.15,2)
-Desa = Product("Desa",3.50,1)
-Siers = Product("Siers",2.50,1)
-
-# print("Olas kopējā cena:",Olas.get_total_price())
-# print("Piens kopējā cena:",Piens.get_total_price())
-
-Grozs = IepirkumuGrozs()
-Grozs.add_product_to_cart(Olas)
-Grozs.add_product_to_cart(Piens)
-Grozs.add_product_to_cart(Desa)
-Grozs.add_product_to_cart(Siers)
-
-Grozs.get_total_price(Olas)
-Grozs.get_total_price(Piens)
-Grozs.get_total_price(Desa)
-Grozs.get_total_price(Siers)
+Desa = Product("Desa",3.50,2)
+Siers = Product("Siers",2.50,5)
 
 
 class IepirkumuGrozs():
-    def __init__(self):
+    def __init__(self,produkts=None):
         self.products = []
     def add_product_to_cart(self, product):
         self.products.append(product)
         print("Produkts", product.name, "Pievienots grozam!")
-    def get_total_price(self, product=None):
-        total_price = 0
-        if product:
-            total_price = product.get_total_price()
-        else:
-            total_price = sum(p.get_total_price() for p in self.products)
+    def get_total_price(self,product=None):
+        total_price = sum(products.price*products.daudzums for products in self.products)
         print("Kopējā summa:", total_price)
-
 
 
 
@@ -61,10 +33,6 @@ Grozs.add_product_to_cart(Piens)
 Grozs.add_product_to_cart(Desa)
 Grozs.add_product_to_cart(Siers)
 
-Grozs.get_total_price(Olas)
-Grozs.get_total_price(Piens)
-Grozs.get_total_price(Desa)
-Grozs.get_total_price(Siers)
 
-# Вывод полной стоимости всех продуктов
+# 'pilnu groza' cenu aprēķins
 Grozs.get_total_price()
